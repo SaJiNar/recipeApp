ba46b856053fdcbe749d000603a6f25cb93e2aff
Sara <sarajnares@gmail.com>
2023-05-10 11:28:45 +0200

    Adding the final parts

--- src/main/project/main.js
+++ src/main/project/main.js
@@ -14,13 +14,14 @@ searchForm.addEventListener('submit', (e) => {
 })
 
 async function fetchApi() {
-    const baseURL = 'https://test-es.edamam.com/search';
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
     const response = await fetch(baseURL);
     const data = await response.json();
     generateHtml(data.hits);
     console.log(data);
 }
 function generateHtml(results){
+    container.classList.remove('initial');
     let generateHTML = '';
     results.map(result => {
         generateHTML +=
@@ -32,7 +33,7 @@ function generateHtml(results){
         <a class = "view-button" href="${result.recipe.url}" target="_blank">View Recipe</a>
     </div>
         <p class = "item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
-        <p class = "item-data">Diet label: ${result.recipe.dietLabel}</p>
+        <p class = "item-data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels.length : 'No Data Found'}</p>
         <p class = "item-data">Health Label: ${result.recipe.healthLabel}</p>
     </div>
         `
